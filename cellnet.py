468c0e30585c3ed682c091c996e795af7aaeb2d6
eckhardt <benjamin.eckhardt@ut.ee>
2024-05-28 15:20:30 +0800

    del results, changes in batch execution

--- cellnet.py
+++ cellnet.py
@@ -1,13 +1,14 @@
 # %% [markdown]
 # # CellNet
-# Overfit 1 image with val augmentations
 
 # %% # Imports 
+IMAGES = 'one'; AUGS = 'test'
+P = 'fraction'; ps = [1]# [0.1, 0.33, 0.5, 0.67, 0.9, 0.95, 1]
+
+
 import ast
 from math import prod
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 import pandas as pd
 import os
 import torch
@@ -44,7 +45,7 @@ XNorm = data.mk_XNorm([1,2,4])
 
 def mkAugs(mode):
   T = lambda ts:  A.Compose(transforms=[
-    A.PadIfNeeded(CROPSIZE*2, CROPSIZE*2, border_mode=0, value=0),
+    A.PadIfNeeded(CROPSIZE, CROPSIZE, border_mode=0, value=0),
     *ts,
     XNorm(), 
     A.PadIfNeeded(CROPSIZE, CROPSIZE, border_mode=0, value=0),
@@ -59,7 +60,9 @@ def mkAugs(mode):
     test  = T([]),
     val   = T([A.RandomCrop(CROPSIZE, CROPSIZE, p=1),
                *vals]),
-    train = T([A.RandomSizedCrop(p=1, min_max_height=(CROPSIZE//2, CROPSIZE*2), height=CROPSIZE, width=CROPSIZE),
+    train = T([
+               A.RandomCrop(CROPSIZE, CROPSIZE, p=1),
+              #A.RandomSizedCrop(p=1, min_max_height=(CROPSIZE//2, CROPSIZE*2), height=CROPSIZE, width=CROPSIZE),
               # NOTE: issue with resize is that the keypoint sizes will not be updated
                A.Rotate(),
                A.AdvancedBlur(),
@@ -72,19 +75,19 @@ def mkAugs(mode):
 
 # %% # Plot data 
 
-def plot_overlay(x,m,z,k, ax=None, sigma=3.5):
+def plot_overlay(x,m,z,k,l, ax=None, sigma=5):
   ax = plot.image(x, ax=ax)
   plot.heatmap(1-m, ax=ax, alpha=lambda x: 0.5*x, color='#000000')
   plot.heatmap(  z, ax=ax, alpha=lambda x: 1.0*x, color='#ff0000')
-  plot.points(ax, k, sigma)
+  plot.points(ax, k, sigma, l)
   return ax
 
-def plot_diff(x,m,y,z,k, ax=None, sigma=3.5):
+def plot_diff(x,m,y,z,k,l, ax=None, sigma=5):
   title = f"Difference between Target and Predicted Heatmap"
   D = y-z; D[0, 1,0] = -1; D[0, 1,1] = 1 
   ax = plot.image(D, ax=ax, cmap='coolwarm')
   plot.heatmap(1-m, ax=ax, alpha=lambda x: 0.2*x, color='#000000')
-  plot.points(ax, k, sigma)
+  plot.points(ax, k, sigma, l)
   return ax
 
 
@@ -95,12 +98,12 @@ if DRAFT and not CUDA:
   def plot_grid(grid, **loader_kwargs):
     loader = data.mk_loader([1], cfg=_cfg, bs=prod(grid), **loader_kwargs)
     B = next(iter(loader))
-    B = zip(B['image'], B['masks'][0], keypoints2heatmap(B), B['keypoints'])
+    B = zip(B['image'], B['masks'][0], keypoints2heatmap(B), B['keypoints'], B['class_labels'])
     for b,ax in zip(B, plot.grid(grid, [CROPSIZE]*2)[1]):
       plot_overlay(*[cpu(v) for v in b], ax=ax)
 
   for B in data.mk_loader([1,2,4], cfg=_cfg, bs=1, transforms=mkAugs('test'), shuffle=False):
-    plot_overlay(*[cpu(v[0]) for v in [B['image'], B['masks'][0], keypoints2heatmap(B), B['keypoints']]])
+    ax = plot_overlay(*[cpu(v[0]) for v in [B['image'], B['masks'][0], keypoints2heatmap(B), B['keypoints'], B['class_labels']]])
 
   plot_grid((3,3), transforms=mkAugs('val'))
   plot_grid((3,3), transforms=mkAugs('train'))
@@ -125,7 +128,7 @@ mk_model()
 
 def accuracy(y,z): 
   ny, nz = y.sum().item(), z.sum().item()
-  return 1 - abs(ny - nz) / nz
+  return 1 - (ny - nz) / nz
 
 def train(epochs, model, optim, lossf, sched, kp2hm, traindl, valdl=None, info={}):
   log = pd.DataFrame(columns='tl vl ta va lr'.split(' '), index=range(epochs))
@@ -149,7 +152,7 @@ def train(epochs, model, optim, lossf, sched, kp2hm, traindl, valdl=None, info={
 
   for e in range(epochs):
     L = log.loc[e]
-    L['lr'] = optim.param_groups[0]['lr']/log.loc[0]['lr'] 
+    L['lr'] = optim.param_groups[0]['lr']/(log.loc[0]['lr'] if log.loc[0]['lr'] else 1) 
   
     model.train()
     L['tl'], L['ta'] = epoch(traindl, train=True)
@@ -165,13 +168,14 @@ def train(epochs, model, optim, lossf, sched, kp2hm, traindl, valdl=None, info={
   return log
 
 
-splits = [([1], [2])]# if DRAFT else [([2,4], [1]), ([1,4], [2]), ([1,2], [4])]
-P = 'fraction'; ps = [1]# if DRAFT else [0.1, 0.3, 0.5, 0.7, 0.9, 0.95, 1]
+splits = [([1], [2])] if DRAFT else dict(
+  one = [([1], [2,4])],
+  all = [([2,4], [1]), ([1,4], [2]), ([1,2], [4])],
+)[IMAGES]
 
 results = pd.DataFrame()
-
 [os.makedirs(p, exist_ok=True) for p in ('preds', 'plots')]
-for p in ps:
+for p in [1] if DRAFT else ps:
   for ti, vi in splits:
     cfg = obj(**(dict(
       sigma=3.5, 
@@ -180,16 +184,16 @@ for p in ps:
       sparsity=1)
       | {P: p}))
     loader = lambda ids, mode: data.mk_loader(ids, bs=1 if mode=='test' else 16, transforms=mkAugs(mode), shuffle=False, cfg=cfg)
-    traindl, valdl = loader(ti, 'val'), loader(vi, 'test')
+    traindl, valdl = loader(ti, AUGS), loader(vi, 'val' if AUGS=='train' else 'test')
     
     kp2hm, yunnorm = data.mk_kp2mh_yunnorm([1,2,4], cfg)
 
     model = mk_model()
-    optim = torch.optim.Adam(model.parameters(), lr=1e-3)
+    optim = torch.optim.Adam(model.parameters(), lr=5e-3)
     lossf = torch.nn.MSELoss()
     sched = torch.optim.lr_scheduler.StepLR(optim, step_size=200, gamma=0.1)
 
-    log = train((10 if CUDA else 2) if DRAFT else 501, model, optim, lossf, sched, kp2hm, traindl, valdl, info={P: p})
+    log = train((5 if CUDA else 1) if DRAFT else 501, model, optim, lossf, sched, kp2hm, traindl, valdl, info={P: p})
 
     _row =  pd.DataFrame(dict(**{P: [p]}, ti=[ti], vi=[vi], **log.iloc[-1]))
     results = _row if results.empty else pd.concat([results, _row], ignore_index=True)
@@ -198,19 +202,23 @@ for p in ps:
     for ii, t in [(ti, 'T'), (vi, 'V')]:
       for i in ii:
         B = next(iter(data.mk_loader([i], bs=1, transforms=mkAugs('test'), shuffle=False, cfg=cfg)))
-        x,m,z,k = [cpu(v[0]) for v in [B['image'], B['masks'][0], kp2hm(B), B['keypoints']]]
+        x,m,z,k,l = [cpu(v[0]) for v in [B['image'], B['masks'][0], kp2hm(B), B['keypoints'], B['class_labels']]]
 
         model.eval()
         with torch.no_grad(): y = cpu(model(B['image'].to(device)))[0]
 
         id = f"{P}={p}-{t}{i}"
         #np.save(f'preds/{id}.npy', y)
-        ax = plot_overlay(x,m,y,k, sigma=cfg.sigma); ax.figure.savefig(f'plots/{id}.pred.png') # type: ignore
-        ax = plot_diff(x,m,y,z,k,  sigma=cfg.sigma); ax.figure.savefig(f'plots/{id}.diff.png') # type: ignore
-        plt.close('all')
+        ax = plot_overlay(x,m,y,k,l, sigma=cfg.sigma); plot.save(ax, f'plots/{id}.pred.png')
+        ax = plot_diff (x,m,y,z,k,l, sigma=cfg.sigma); plot.save(ax, f'plots/{id}.diff.png')
+        #plt.close('all')
 
     
 # %% # save the results as csv. exclude model column; plot accuracies
-results.to_csv('results.csv', index=False, sep=';')
-R = pd.read_csv('results.csv', sep=';', converters=dict(ti=ast.literal_eval, vi=ast.literal_eval)).rename(columns=dict(vi=key2text['vi']))
-plot.regplot(R, P, key2text)
+
+if not DRAFT:
+  results.to_csv('results.csv', index=False, sep=';')
+  R = pd.read_csv('results.csv', sep=';', converters=dict(ti=ast.literal_eval, vi=ast.literal_eval)).rename(columns=dict(vi=key2text['vi']))
+  plot.regplot(R, P, key2text)
+
+results
