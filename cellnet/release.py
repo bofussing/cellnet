41b9d9f49e6b566827583b0445824c9d7669bee4
eckhardt <benjamin.eckhardt@ut.ee>
2024-07-10 18:56:39 +0300

    v0.2.0
    removed data from repo
    LICENSEing
    plotting in release
    versioning

--- cellnet/release.py
+++ cellnet/release.py
@@ -4,42 +4,39 @@ import segmentation_models_pytorch as smp
 from PIL import Image
 import numpy as np
 
-import json
-import requests, zipfile, tqdm, shutil, os
+import json, zipfile, shutil, os, sys
+
+import cellnet
+import cellnet.plot as plotting
+from cellnet.internet import download, GHAPI
 
 
-def get_latest_release(repo='beijn/cellnet'):
-  return requests.get(f"https://api.github.com/repos/{repo}/releases/latest").json()["tag_name"]
+GH = GHAPI('beijn/cellnet')
 
-def download(url, filename, overwrite=False):
-  if os.path.isdir(filename): filename = os.path.join(filename, url.split('/')[-1])
-  if not overwrite and os.path.isfile(filename): return print("Loading cached model.")
-  os.makedirs(os.path.dirname(filename), exist_ok=True)
-  try: 
-    with open(filename, 'wb') as f:   
-      with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        total = int(r.headers.get('content-length', 0))
-
-        with tqdm.tqdm(desc='Caching model', total=total, unit='B', unit_scale=True, unit_divisor=1024) as pb:
-          for chunk in r.iter_content(chunk_size=8192):
-            f.write(chunk)
-            pb.update(len(chunk))
-  except Exception as e:
-    os.remove(filename)
-    raise e
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+                            
   
+def get_latest_compatible_model_version():
+  try: versions = GH.get_all_releases()
+  except Exception as e: sys.exit(print(f'Could not get the latest release from GH API. Please specify manually in init_model(<version>). See available versions at https://github.com/beijn/cellnet/releases.\n\n{e}'))
+
+  compatible_versions = [v for v in versions if v.split('.')[0]==cellnet.__model_api_version__]
+  if not compatible_versions: sys.exit(print(f"No compatible model (model api version {cellnet.__model_api_version__}) found at https://github.com/beijn/cellnet/releases. \nPlease specify manually in init_model(<version>). \nFound incompatible versions: {versions}"))
+
+  return compatible_versions[0]
+
 
 # TODO: restrict to compatible versions?
-def init_model(version:str='latest', keep_download_cache=True, overwrite_location=None):
+def init_model(version:str|None='latest', keep_download_cache=True):
   cache = os.path.expanduser('~/.cache/cellnet')
-  modeldir = f'{cache}/model_export'
-  if overwrite_location: modeldir = overwrite_location
+  modeldir = version if type(version) is str and os.path.isdir(version) \
+        else f'{cache}/model_export'
   versionfile = f'{modeldir}/version.json'
 
   # if the version is None, we just use whatever is cached or redownload the latest if it's not cached
-  if not (overwrite_location or (version == None and os.path.isdir(modeldir))): 
-    if version == 'latest': version = get_latest_release()
+  if not (version == None and os.path.isdir(modeldir)): 
+    if version == None: version = 'latest'
+    if version == 'latest': version = get_latest_compatible_model_version()
 
     prexisting_version = json.load(open(versionfile))['version'] if os.path.isfile(versionfile) else None
     if prexisting_version != version:
@@ -56,39 +53,56 @@ def init_model(version:str='latest', keep_download_cache=True, overwrite_locatio
   model = smp.Unet.from_pretrained(f'{modeldir}')
   setattr(model, 'pipline_settings', pipeline_settings)
   setattr(model, 'version', version)
-  return model
+  print('Model loaded version:', version)
+  return model.to(DEVICE)
 
-def load_image(image_file_descriptor, pipline_settings):
-  mean, std = [np.array(pipline_settings[k], dtype=np.float32)  * 255   for k in ('xmean', 'xstd')]
-  return ((np.array(Image.open(image_file_descriptor)) - mean) / std).transpose(2, 0, 1)
+def load_image(image_file_descriptor, pipline_settings, norm_with_train_statistics=True):
+  X = np.array(Image.open(image_file_descriptor))
+  if norm_with_train_statistics:
+    mean, std = [np.array(pipline_settings[k], dtype=np.float32)  * 255   for k in ('xmean', 'xstd')]
+  else:  # calculate mean and std from the image 
+    mean, std = X.mean(axis=(0,1)), X.std(axis=(0,1))
+  return ((X - mean) / std).transpose(2, 0, 1)
 
-def count(images, model=None):
+def count(images, model=None, plot=True, norm_with_train_statistics=True):
   if model is None or type(model) == str:  
-    model = init_model('latest' if model is None else model)
+    model = init_model(model)
   model.eval()  # important
 
-  counts = {}
+  counts = {}; plots = {}
   for image in images:
-    X = load_image(image, model.pipline_settings)[None]
-    Y = model(torch.tensor(X).float()).detach().cpu().numpy()
+    X = load_image(image, model.pipline_settings, norm_with_train_statistics)[None]
+    Y = model(torch.tensor(X).float().to(DEVICE)).detach().cpu().numpy()
 
     counts[image.name] = np.sum(Y)*model.pipline_settings["ymax"]
+    if plot: plots[image.name] = plotting.overlay(X[0], Y[0])
 
-  return counts
+  return counts, plots
 
 
 if __name__ == '__main__':
-  import sys
-  import os
-  folder = sys.argv[1]
-  image_paths = [f"{folder}/{f}" for f in os.listdir(folder) if f.endswith('.jpg')]
+  target = sys.argv[1]
+  model_version = sys.argv[2] if len(sys.argv) > 2 else None
 
-  model = init_model()
+  # if target is a folder 
+  if os.path.isdir(target):
+    image_paths = [f"{target}/{f}" for f in os.listdir(target) if f.endswith('.jpg')]
+  elif os.path.isfile(target):
+    image_paths = [target]
+  else: raise ValueError(f"Could not find target: {target}") 
+
+  model = init_model(model_version)
 
   counts = {}
+  os.makedirs('plots', exist_ok=True)
   for path in image_paths: 
     with open(path, 'rb') as f:
-      counts |= count([f], model)
+      cs, plots = count([f], model, plot=True, norm_with_train_statistics=False)
+      counts |= cs 
+      plotting.save(plots[path], os.path.join('plots', os.path.basename(path.removesuffix('.jpg')+'.png')))
 
   print(counts)
+
+  if os.path.isfile('counts.json'): 
+    counts = json.load(open('counts.json')) | counts
   json.dump(counts, open('counts.json', 'w'), indent=2, sort_keys=True)
--- eval.py
+++
@@ -1,3 +0,0 @@
-# %% [markdown]
-# # Cellnet Inference
-
